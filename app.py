dd8407bf1e5e7097d9a32ddf59280175223ba81e
Muhamad Nur Yasin Amadudin <forsvive22@gmail.com>
2025-02-02 05:42:48 +0000

    add Procfile and test Deploy Heroku

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # app.py
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -7,4 +8,4 @@ def hello():
     return "Hello DevOps World!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
